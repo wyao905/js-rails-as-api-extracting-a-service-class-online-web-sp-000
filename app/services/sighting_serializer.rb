29eb893bc7ff435bdee5faa47aadf90bb885b6b1
Warren Yao <warrenyca905@gmail.com>
2020-01-23 00:18:22 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -4,9 +4,18 @@ class SightingSerializer
   end
   
   def to_serialized_json
-    @sighting.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
+    options = {
+      include: {
+        bird: {
+          only: [:name, :species]
+        },
+        location: {
+          only: [:latitude, :longitude]
+        }
+      },
+      except: [:updated_at],
+    }
+    
+    @sighting.to_json(options)
   end
 end
